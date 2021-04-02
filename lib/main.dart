254cba96a78a17792dfbea89059878e833cfdb3c
Endeavor36 <allmight524@gmail.com>
2021-04-02 12:15:19 +0530

    minor changes to theme data

--- lib/main.dart
+++ lib/main.dart
@@ -8,11 +8,10 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-          primaryColor: Color(0xFF0A0D22),
-          scaffoldBackgroundColor: Color(0xFF0A0D22),
-          accentColor: Colors.purple,
-          textTheme: TextTheme(bodyText2: TextStyle(color: Color(0xFFFFFFFF)))),
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xFF0A0E21),
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
+      ),
       home: InputPage(),
     );
   }
