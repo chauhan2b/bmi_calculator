5a1c1b19732a094f124a9348d6a3816b0e732241
Endeavor36 <allmight524@gmail.com>
2021-04-03 10:32:54 +0530

    minor changes

--- lib/screens/input_page.dart
+++ lib/screens/input_page.dart
@@ -4,10 +4,10 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import '../components/icon_content.dart';
 import '../components/reusable_card.dart';
-import '../constants.dart';
-import '../components/icon_button.dart';
-import './results_page.dart';
 import '../components/bottom_button.dart';
+import '../components/icon_button.dart';
+import '../constants.dart';
+import './results_page.dart';
 
 enum Gender { male, female }
 
